9241beca0d140d5402bec39d70960e137b9b5c0f
André Fabiano <andrefsr1902@gmail.com>
2026-07-08 10:30:27 -0300

    Matriz LIFT funcionando perfeitamente

--- operators2D.py
+++ operators2D.py
@@ -85,7 +85,6 @@ def Filter2D(N, Nc, s, V):
     N  : Ordem máxima do polinômio
     Nc : Modo de corte (modos abaixo de Nc não são filtrados)
     s  : Ordem do filtro (força da atenuação, deve ser um número par, ex: 2, 4, 6...)
-    V  : Matriz de Vandermonde 2D do elemento
     """
     # alpha é definido com base no zero da máquina para que o último modo 
     # seja atenuado até o limite de precisão do computador (aprox. 36.04)
@@ -114,3 +113,63 @@ def Filter2D(N, Nc, s, V):
     
     return F
 
+import numpy as np
+
+def Lift2D(N, r, s, V):
+    '''Compute surface to volume lift term for DG formulation'''
+    # Quando a simulação estiver rodando no loop de tempo,
+    # você vai calcular a diferença da onda entre um triângulo e seu vizinho (o fluxo na borda) e multiplicar por essa matriz LIFT.
+    # Ela automaticamente espalhará a correção do fluxo para os nós internos do triângulo
+    
+    #Constrói a matriz de índices Fmask para o triângulo de referência.
+    #Retorna uma matriz onde cada coluna contém os índices dos nós 
+    #pertencentes a uma das faces.
+
+    tol=1e-10
+
+    # 1. Encontra os índices (IDs) dos pontos que satisfazem a geometria da face
+    # O [0] no final serve para extrair o array de dentro da tupla que o np.where retorna
+    fmask1 = np.where(np.abs(s + 1.0) < tol)[0]  # Face 1: s = -1
+    fmask2 = np.where(np.abs(r + s) < tol)[0]    # Face 2: r + s = 0
+    fmask3 = np.where(np.abs(r + 1.0) < tol)[0]  # Face 3: r = -1
+    
+    # 2. Agrupa as três listas como colunas em uma matriz 2D
+    # O resultado será uma matriz de tamanho (Nfp, 3)
+    Fmask = np.column_stack((fmask1, fmask2, fmask3))
+
+    Np = ((N + 1) * (N + 2)) // 2  # Divisão inteira
+    Nfp = N + 1                    # Número de nós na face (borda)
+    Nfaces = 3                     # Triângulo tem 3 faces
+    
+    Emat = np.zeros((Np, Nfaces * Nfp))
+    
+    # --- FACE 1 (Aresta Inferior) ---
+    faceR = r[Fmask[:, 0]]
+    V1D = aux.Vandermonde1D(N, faceR)
+    massEdge1 = np.linalg.inv(V1D @ V1D.T)
+    # Insere os blocos na matriz usando a indexação correta do Python
+    Emat[Fmask[:, 0], 0:Nfp] = massEdge1
+    
+    # --- FACE 2 (Aresta Direita) ---
+    faceR = r[Fmask[:, 1]]
+    V1D = aux.Vandermonde1D(N, faceR)
+    massEdge2 = np.linalg.inv(V1D @ V1D.T)
+    Emat[Fmask[:, 1], Nfp:2*Nfp] = massEdge2
+    
+    # --- FACE 3 (Aresta Esquerda) ---
+    faceS = s[Fmask[:, 2]] # Aqui usamos s, pois r é constante na face esquerda
+    V1D = aux.Vandermonde1D(N, faceS)
+    massEdge3 = np.linalg.inv(V1D @ V1D.T)
+    Emat[Fmask[:, 2], 2*Nfp:3*Nfp] = massEdge3
+    
+    # LIFT = Matriz de Massa Inversa * Emat
+    LIFT = V @ (V.T @ Emat)
+    
+    return LIFT
+
+
+
+
+
+
+    
